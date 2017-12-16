c14af746c982111e9417735a788121d98b0a6cdc
matt <matthewneverhart@gmail.com>
2017-12-16 13:22:39 -0500

    working version of conway's game of life

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,95 +9,98 @@ var width1 = gameCanvas1.width;
 var height1 = gameCanvas1.height;
 
 //initialize the grid
-var grid = girdInit(20,20,20,'#0b175b',"#2b43c6");
+var grid = girdInit(20,30,30,'#0b175b',"#2b43c6");
 //draw the gird
 drawGrid(context1, grid, true);
 
 //this captures the clicks on the canvas and turns on cells. 
 $("body").click(gameCanvas1,function(e){
 	var mousePos = getMousePos(gameCanvas1, e);
-	console.log(mousePos.x+":"+mousePos.y);
 	var foundCell = findCellAt(grid, mousePos.x, mousePos.y);
 	//check to make sure we're not off the canvas...
-	if(foundCell != undefined){
+	if(foundCell != undefined && (foundCell.fillColor == "#0b175b")){
 		foundCell.fillColor = "#fff";
+		drawGrid(context1, grid, true);
+	}
+	else if(foundCell != undefined && (foundCell.fillColor == "#fff")){
+		foundCell.fillColor = "#0b175b";
 		drawGrid(context1, grid, true);
 	}
 });
 
-async function gameOfLife(grid, runs, deadColor, liveColor){
+async function runLife(grid){
+	var runs = parseInt($("#ticks").val(), 10);
+	if(isNaN(runs)){runs = 1}; //if we dont enter a number, or something else dumb.
 	
-	/* //this runs the simulation forward 'runs' times.
-	for(var j=0;j<runs;j++){ */
-		
-		//first we make a temp grid and copy the living or dead status from the real grid
-		//this way, we can change the temp grid later on, without disrupting the real grid and skewing neighbor counts. 
-		//and then make the real grid match the temp grid's changes.
-		var tempGrid = grid;
-		console.log(grid);
-		
+	//this runs the simulation forward 'runs' times.
+	for(var j=0;j<runs;j++){
+		//for some reason this isnt showing every step. it just jumps right to the last output. need to work on it. 
+		await gameOfLife(grid, '#0b175b', '#fff');
+	}//end runs loop
+}
+
+async function gameOfLife(grid, deadColor, liveColor){
+	
+		//we have two sets of loops here. the first just counts the neighbors, the second turns the cells on or off.
+		//I do this to avoid turning cells on and off while still trying to count neighbors.
 		for(var i =0;i<grid.length;i++){
 			for(var k=0;k<grid[i].length;k++){
-					var liveNeighbors = 0;
 					//first we find the live neighbors
 				
 					 if(k-1 >= 0){//check the north neighbor, if he exists
-						if(grid[i][k-1].fillColor == liveColor){liveNeighbors++};
+						if(grid[i][k-1].fillColor == liveColor){grid[i][k].liveNeighbors++};
 					}
 					if(i-1 >= 0 && k-1 >= 0){ //check the north western neighbor, if he exists. 
-						if(grid[i-1][k-1].fillColor == liveColor){liveNeighbors++};
+						if(grid[i-1][k-1].fillColor == liveColor){grid[i][k].liveNeighbors++};
 					}
 					if(i+1 <= grid.length-1 && k-1 >= 0){ //check the north eastern neighbor, if he exists
-						if(grid[i+1][k-1].fillColor == liveColor){liveNeighbors++};
+						if(grid[i+1][k-1].fillColor == liveColor){grid[i][k].liveNeighbors++};
 					}
 					if(k+1 <= grid[i].length-1){ //check the south neighbor if he exists
-						if(grid[i][k+1].fillColor == liveColor){liveNeighbors++};
+						if(grid[i][k+1].fillColor == liveColor){grid[i][k].liveNeighbors++};
 					}
 					if(k+1 <= grid[i].length-1 && i-1 >= 0){ //check the south western neighbor, if he exists. same as north west.
-						if(grid[i-1][k+1].fillColor == liveColor){liveNeighbors++};
+						if(grid[i-1][k+1].fillColor == liveColor){grid[i][k].liveNeighbors++};
 					}
 					if(k+1 <= grid[i].length-1 && i+1 <= grid.length-1){ //check the south eastern neighbor if he exists
-						if(grid[i+1][k+1].fillColor == liveColor){liveNeighbors++};
+						if(grid[i+1][k+1].fillColor == liveColor){grid[i][k].liveNeighbors++};
 					}
 					if(i-1 >= 0){ //check the west neighbor if he exists
-						if(grid[i-1][k].fillColor == liveColor){liveNeighbors++};
+						if(grid[i-1][k].fillColor == liveColor){grid[i][k].liveNeighbors++};
 					}
 					if(i+1 <= grid.length-1){ //check the east neighbor if he exists
-						if(grid[i+1][k].fillColor == liveColor){liveNeighbors++};
+						if(grid[i+1][k].fillColor == liveColor){grid[i][k].liveNeighbors++};
 					}
 					
-					//depending on the number of live neighbors we turn this cell on or off: TODO - this is broken. check rules and verify conditionals
-					if(grid[i][k].fillColor == liveColor && liveNeighbors < 2){
+			}//end y for
+		}//end x for
+					
+		//this set of loops turns the cells on or off.
+		for(var i =0;i<grid.length;i++){
+			for(var k=0;k<grid[i].length;k++){
+					//depending on the number of live neighbors we turn this cell on or off
+					if(grid[i][k].fillColor == liveColor && grid[i][k].liveNeighbors < 2){
 						//Any live cell with fewer than two live neighbours dies, as if caused by underpopulation.
-						tempGrid[i][k].fillColor = deadColor;
+						grid[i][k].fillColor = deadColor;
 					}
-					else if(grid[i][k].fillColor == liveColor && (liveNeighbors == 3 || liveNeighbors == 2)){
+					else if(grid[i][k].fillColor == liveColor && (grid[i][k].liveNeighbors == 3 || grid[i][k].liveNeighbors == 2)){
 						//Any live cell with two or three live neighbours lives on to the next generation.
-						tempGrid[i][k].fillColor = liveColor;
+						grid[i][k].fillColor = liveColor;
 					}
-					else if(grid[i][k].fillColor == liveColor && liveNeighbors > 3){
+					else if(grid[i][k].fillColor == liveColor && grid[i][k].liveNeighbors > 3){
 						//Any live cell with more than three live neighbours dies, as if by overpopulation.
-						tempGrid[i][k].fillColor = deadColor;
+						grid[i][k].fillColor = deadColor;
 					}	
-					else if(liveNeighbors == 3 && grid[i][k].fillColor == deadColor){
+					else if(grid[i][k].liveNeighbors == 3 && grid[i][k].fillColor == deadColor){
 						//Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
-						tempGrid[i][k].fillColor = liveColor;
+						grid[i][k].fillColor = liveColor;
 					}
+					grid[i][k].liveNeighbors = 0;//reset the liveNeighbor count after using the info.
 			}//end y for loop
 		}//end x for loop
 		
-		//now we make the real grid match the changes in the temp grid. 
-		grid = tempGrid;
-		/* for(var i =0;i<grid.length;i++){
-			for(var k=0;k<grid[i].length;k++){
-				grid[i][k].fillColor = tempGrid[i][k].fillColor;
-			}
-		} */
-		
 		//and redraw the real grid with all the new changes. 
 		await drawGrid(context1, grid, true);
-		
-	//}//end runs loop
 };
 
 
@@ -201,13 +204,13 @@ function drawGrid(canvas, grid, drawWalls)
 			 }//end drawWalls check.
 			}//end x for loop
 		  }//end y for loop
+		  
 		 resolve(true);
-		 console.log("grid drawn. promise resolved");
 	});//end promise
 	
 }//end drawGrid
 
-//this is the cell constructor. it represents the info for each cell on our grid.
+//this is the cell constructor. it represents and helps set up the info for each cell on our grid.
 function Cell(x, y, h, w, indexX, indexY, fillColor, edgeColor)
 {
   this.x = x; //this is the x pixel location of the top left corner of this cell on the canvas object
@@ -222,6 +225,7 @@ function Cell(x, y, h, w, indexX, indexY, fillColor, edgeColor)
   this.wallS = true; //is this cell's south wall up?
   this.wallW = true; //is this cell's west wall up?
   this.beenTo = false; //have we been to this cell before?
+  this.liveNeighbors = 0; //how many of this cell's neighbors are alive?
   this.fillColor = fillColor;
   this.edgeColor = edgeColor;
 }//end constructor
@@ -238,7 +242,6 @@ function findCellAt(grid, x, y){
 			
 			//if the passed in x and y are within the height and width of this cell's x and y, its a match.
 			if((x >= cellXmin && x <= cellXmax) && (y >= cellYmin && y <= cellYmax)){
-				console.log(grid[i][k]);
 				return grid[i][k];
 			}
 		}
